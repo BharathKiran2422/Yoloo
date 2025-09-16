759b2e6ae57c29bcef5c1265336b4304c09591b6
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-09-16 20:08:02 +0000

    no change in dark theme for the logo

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex items-center font-bold text-2xl text-primary dark:text-primary-foreground", className)}>
+    <div className={cn("flex items-center font-bold text-2xl text-primary dark:text-foreground", className)}>
         <span>YOLOO!</span>
     </div>
   );
