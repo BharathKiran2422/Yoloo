24fe5ac1e7b3231d29ddeff55f0e194ea5009b39
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-09-17 13:21:31 +0000

    direct to download section (_for element button_)

--- src/components/sections/just-do-it-section.tsx
+++ src/components/sections/just-do-it-section.tsx
@@ -7,7 +7,7 @@ export function JustDoItSection() {
             <div className="container mx-auto text-center">
                 <p className="text-lg text-muted-foreground">Only one way to find out.</p>
                 <h2 className="text-6xl md:text-8xl font-black my-4 text-foreground tracking-tighter">JUST DO IT.</h2>
-                <Link href="/men">
+                <Link href="#purchase-on-app">
                     <Button size="lg" className="rounded-full bg-foreground text-background hover:bg-foreground/80 px-10">
                         Shop
                     </Button>
