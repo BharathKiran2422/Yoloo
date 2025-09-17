cd7b993359a4c850997d9e7b0c584598a1a97a9a
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-09-17 16:59:45 +0000

    Update the website’s global background (applied in layout.tsx or main Ap

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,10 +69,15 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'gradient-move': {
+          '0%': { 'background-position': '0% 0%' },
+          '100%': { 'background-position': '100% 100%' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'gradient-move': 'gradient-move 20s ease-in-out infinite alternate',
       },
     },
   },
