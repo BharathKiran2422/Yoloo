0446d464a87cd753b8849ad75f35db8db5d53e03
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-09-25 20:22:48 +0000

    iam having the same sidebar issue only when iam opening sidebar in "/" p

--- src/components/brand-carousel.tsx
+++ src/components/brand-carousel.tsx
@@ -18,7 +18,7 @@ export function BrandCarousel() {
     <div className="py-8 md:py-12 bg-background overflow-hidden relative">
        <div className="absolute inset-y-0 left-0 w-16 md:w-24 bg-gradient-to-r from-background to-transparent z-10"></div>
       <div
-        className="w-max"
+        className="group flex"
         x-data="{}"
         x-init="$el.classList.add('animate-marquee')"
       >
