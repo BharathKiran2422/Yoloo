3491cb85169f39a1fda65b13e4f4e19c3c36657f
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-10-27 07:14:28 +0000

    make the layout spread horizontal and make the lines represented in 2lin

--- src/app/comingsoon/page.tsx
+++ src/app/comingsoon/page.tsx
@@ -102,7 +102,7 @@ export default function ComingSoonPage() {
                         animate={{ y: 0, opacity: 1 }}
                         transition={{ delay: 0.4, duration: 0.5 }}
                     >
-                       When style needs you. <br className="md:hidden" /> When you need style. <br /> Yoloo!
+                       When style needs you. When you need style. <br /> Yoloo!
                     </motion.h1>
 
                     <motion.p 
