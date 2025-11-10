791b6688b17717cf19dac52e4a0c11c329059f0d
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-11-10 16:58:27 +0000

    remove page refershment to the men, women, sneakers, accessories pages

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -204,12 +204,13 @@ export function Header() {
 const ListItem = forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+>(({ className, title, children, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
+          href={href || '#'}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
@@ -220,9 +221,10 @@ const ListItem = forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
 })
 ListItem.displayName = "ListItem"
+
